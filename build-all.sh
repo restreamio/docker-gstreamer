f57a51eccd7ed33b622a2a4bb9889ea72c40d260
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2020-03-29 01:17:15 +0200

    feat: make things build in more separate images for ability to provide more final images, script to push images built

--- build-all.sh
+++ build-all.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 set -e
 
+# Make sure to always have fresh base image
 docker pull ubuntu:19.10
+# Install dev dependencies
+docker build -t restreamio/gstreamer:latest-dev-dependencies -f Dockerfile-dev-dependencies .
+# Download source code
+docker build -t restreamio/gstreamer:latest-dev-downloaded -f Dockerfile-dev-downloaded .
+# Build dev image with source code included
+docker build -t restreamio/gstreamer:latest-dev-with-source -f Dockerfile-dev-with-source .
+# Build dev image with just binaries
 docker build -t restreamio/gstreamer:latest-dev -f Dockerfile-dev .
+# Build production image with just binaries and necessary dependencies
 docker build -t restreamio/gstreamer:latest-prod -f Dockerfile-prod .
