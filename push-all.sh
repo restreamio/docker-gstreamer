5b94c1b7997071ef19df604deffe05924fbf0e46
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2020-05-27 19:32:04 +0300

    feat: also push tags with date for stable reference

--- push-all.sh
+++ push-all.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 set -e
 
+DATE=$(date -u +"%Y-%m-%dT%H-%M-%SZ")
+
 docker push restreamio/gstreamer:latest-dev-with-source
+docker tag restreamio/gstreamer:latest-dev-with-source restreamio/gstreamer:$DATE-latest-dev-with-source
+docker push restreamio/gstreamer:$DATE-latest-dev-with-source
+
 docker push restreamio/gstreamer:latest-dev
+docker tag restreamio/gstreamer:latest-dev restreamio/gstreamer:$DATE-latest-dev
+docker push restreamio/gstreamer:$DATE-latest-dev
+
 docker push restreamio/gstreamer:latest-prod
+docker tag restreamio/gstreamer:latest-prod restreamio/gstreamer:$DATE-latest-prod
+docker push restreamio/gstreamer:$DATE-latest-prod
+
 docker push restreamio/gstreamer:latest-prod-dbg
+docker tag restreamio/gstreamer:latest-prod-dbg restreamio/gstreamer:$DATE-latest-prod-dbg
+docker push restreamio/gstreamer:$DATE-latest-prod-dbg
+
