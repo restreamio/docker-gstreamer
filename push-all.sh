924cd61ebbc2f7b0ead37781f47fb37075f2eaae
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2020-05-27 23:00:24 +0300

    fix: shorter date-prefixed image tags

--- push-all.sh
+++ push-all.sh
@@ -4,18 +4,18 @@ set -e
 DATE=$(date -u +"%Y-%m-%dT%H-%M-%SZ")
 
 docker push restreamio/gstreamer:latest-dev-with-source
-docker tag restreamio/gstreamer:latest-dev-with-source restreamio/gstreamer:$DATE-latest-dev-with-source
-docker push restreamio/gstreamer:$DATE-latest-dev-with-source
+docker tag restreamio/gstreamer:latest-dev-with-source restreamio/gstreamer:$DATE-dev-with-source
+docker push restreamio/gstreamer:$DATE-dev-with-source
 
 docker push restreamio/gstreamer:latest-dev
-docker tag restreamio/gstreamer:latest-dev restreamio/gstreamer:$DATE-latest-dev
-docker push restreamio/gstreamer:$DATE-latest-dev
+docker tag restreamio/gstreamer:latest-dev restreamio/gstreamer:$DATE-dev
+docker push restreamio/gstreamer:$DATE-dev
 
 docker push restreamio/gstreamer:latest-prod
-docker tag restreamio/gstreamer:latest-prod restreamio/gstreamer:$DATE-latest-prod
-docker push restreamio/gstreamer:$DATE-latest-prod
+docker tag restreamio/gstreamer:latest-prod restreamio/gstreamer:$DATE-prod
+docker push restreamio/gstreamer:$DATE-prod
 
 docker push restreamio/gstreamer:latest-prod-dbg
-docker tag restreamio/gstreamer:latest-prod-dbg restreamio/gstreamer:$DATE-latest-prod-dbg
-docker push restreamio/gstreamer:$DATE-latest-prod-dbg
+docker tag restreamio/gstreamer:latest-prod-dbg restreamio/gstreamer:$DATE-prod-dbg
+docker push restreamio/gstreamer:$DATE-prod-dbg
 
