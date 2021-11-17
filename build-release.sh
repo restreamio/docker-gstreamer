3116059a0babc292b1a93c8fa52d6a0a376d3cb2
Philippe Normand <philn@igalia.com>
2021-11-17 17:46:11 +0000

    latest: Port to GStreamer mono-repo
    The patches scattered in Nazar's forks were gathered in my GStreamer mono-repo
    fork. Libnice is built as a GStreamer subproject now. If we need to bump its
    revision it can be done through the corresponding .wrap file in my fork.

--- build-release.sh
+++ build-release.sh
@@ -2,7 +2,7 @@
 set -e
 
 if [[ -z "$1" ]]; then
-    echo -e "Usage example:\n  $0 1.18.1"
+    echo -e "Usage example:\n  $0 1.20.1"
     exit 1
 fi
 
@@ -14,18 +14,6 @@ docker build -t restreamio/gstreamer:dev-dependencies -f Dockerfile-dev-dependen
 docker build -t restreamio/gstreamer:dev-downloaded \
     --build-arg GSTREAMER_REPOSITORY=https://gitlab.freedesktop.org/gstreamer/gstreamer.git \
     --build-arg GSTREAMER_CHECKOUT=$1 \
-    --build-arg GST_PLUGINS_BASE_REPOSITORY=https://gitlab.freedesktop.org/gstreamer/gst-plugins-base.git \
-    --build-arg GST_PLUGINS_BASE_CHECKOUT=$1 \
-    --build-arg GST_PLUGINS_BAD_REPOSITORY=https://gitlab.freedesktop.org/gstreamer/gst-plugins-bad.git \
-    --build-arg GST_PLUGINS_BAD_CHECKOUT=$1 \
-    --build-arg GST_PLUGINS_GOOD_REPOSITORY=https://gitlab.freedesktop.org/gstreamer/gst-plugins-good.git \
-    --build-arg GST_PLUGINS_GOOD_CHECKOUT=$1 \
-    --build-arg GST_PLUGINS_UGLY_REPOSITORY=https://gitlab.freedesktop.org/gstreamer/gst-plugins-ugly.git \
-    --build-arg GST_PLUGINS_UGLY_CHECKOUT=$1 \
-    --build-arg GST_LIBAV_REPOSITORY=https://gitlab.freedesktop.org/gstreamer/gst-libav.git \
-    --build-arg GST_LIBAV_CHECKOUT=$1 \
-    --build-arg GSTREAMER_VAAPI_REPOSITORY=https://gitlab.freedesktop.org/gstreamer/gstreamer-vaapi.git \
-    --build-arg GSTREAMER_VAAPI_CHECKOUT=$1 \
     -f Dockerfile-dev-downloaded .
 # Build dev image with source code included
 docker build -t restreamio/gstreamer:$1-dev-with-source -f Dockerfile-dev-with-source .
