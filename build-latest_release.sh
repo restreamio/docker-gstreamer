59b7b89de30fe00e281dca0f94f8379c9253d76d
Athreya Rajaraman <athreya.rajaraman@spectricity.com>
2022-02-10 17:49:33 +0100

    to be tested

---
+++ build-latest_release.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+export LATEST_RELEASE=1.20
+# Make sure to always have fresh base image
+docker pull ubuntu:21.04
+# Install dev dependencies
+docker build -t restreamio/gstreamer:dev-dependencies -f Dockerfile-dev-dependencies .
+# Download source code
+docker build -t restreamio/gstreamer:dev-downloaded \
+    --build-arg GSTREAMER_REPOSITORY=https://gitlab.freedesktop.org/gstreamer/gstreamer.git \
+    --build-arg GSTREAMER_CHECKOUT=$LATEST_RELEASE \
+    -f Dockerfile-dev-mono-repo-downloaded .
+# Build dev image with source code included
+docker build -t restreamio/gstreamer:latest-dev-with-source -f Dockerfile-dev-with-source .
+# Build dev image with just binaries
+docker build -t restreamio/gstreamer:latest-dev -f Dockerfile-dev .
+# Build base production image with necessary dependencies
+docker build -t restreamio/gstreamer:prod-base -f Dockerfile-prod-base .
+# Build production image optimized binaries and no debug symbols (-O3 LTO)
+docker build -t restreamio/gstreamer:latest-prod -f Dockerfile-prod .
+# Build production image optimized binaries and debug symbols
+docker build -t restreamio/gstreamer:latest-prod-dbg -f Dockerfile-prod-dbg .
