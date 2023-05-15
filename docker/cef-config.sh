7ae82594215ec3857388286aaec4a852adc0fcee
ArmorDarks <git@lavrins.com>
2023-05-15 13:54:56 +0100

    feat: update CEF download URL
    CEF was moved to the other place. No changes in files.

--- docker/cef-config.sh
+++ docker/cef-config.sh
@@ -1,3 +1,3 @@
 
-export CEF_DOWNLOAD_URL=https://restream-archive.s3.eu-west-1.amazonaws.com/cef
+export CEF_DOWNLOAD_URL=https://download.restream.io/gstreamer/cef
 export CEF_VERSION=106.1.1+g5891c70+chromium-106.0.5249.119
