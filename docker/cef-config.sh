77cd3a60662fe402a21e0090a50be8a5de4141e7
Alex Kosenko <alex.kosenko@restream.io>
2024-01-18 22:22:35 -0500

    Check old chromium 106

--- docker/cef-config.sh
+++ docker/cef-config.sh
@@ -1,5 +1,5 @@
 
 #export CEF_DOWNLOAD_URL=https://download.restream.io/gstreamer/cef
-#export CEF_VERSION=106.1.1+g5891c70+chromium-106.0.5249.119
 export CEF_DOWNLOAD_URL=https://cef-builds.spotifycdn.com
-export CEF_VERSION=120.2.3+g7d89c0c+chromium-120.0.6099.199
+export CEF_VERSION=106.1.1+g5891c70+chromium-106.0.5249.119
+#export CEF_VERSION=120.2.3+g7d89c0c+chromium-120.0.6099.199
