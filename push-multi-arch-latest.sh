63f841c61a4c549ff7b8cc05b7736e4bfafdd026
Roman Slipchenko <roman.slipchenko@restream.io>
2024-12-11 20:24:13 +0200

    ci: fix path to tag files

--- push-multi-arch-latest.sh
+++ push-multi-arch-latest.sh
@@ -5,8 +5,13 @@ echo $DATE
 
 TAG_BASENAME="restreamio/gstreamer:$DATE"
 
-AMD64_TAG_BASENAME=$(cat /tmp/docker-gst-ci/workspace/docker-tag-basename-x86_64.txt)
-ARM64_TAG_BASENAME=$(cat /tmp/docker-gst-ci/workspace/docker-tag-basename-aarch64.txt)
+echo "basename tag: $TAG_BASENAME"
+
+AMD64_TAG_BASENAME=$(cat ./workspace/docker-tag-basename-x86_64.txt)
+ARM64_TAG_BASENAME=$(cat ./workspace/docker-tag-basename-aarch64.txt)
+
+echo "amd64 basename tag: $AMD64_TAG_BASENAME"
+echo "arm64 basename tag: $ARM64_TAG_BASENAME"
 
 docker manifest create $TAG_BASENAME-dev-with-source --amend $AMD64_TAG_BASENAME-dev-with-source --amend $ARM64_TAG_BASENAME-dev-with-source
 docker manifest push docker.io/$TAG_BASENAME-dev-with-source
@@ -19,4 +24,3 @@ docker manifest push docker.io/$TAG_BASENAME-prod
 
 docker manifest create $TAG_BASENAME-prod-dbg --amend $AMD64_TAG_BASENAME-prod-dbg --amend $ARM64_TAG_BASENAME-prod-dbg
 docker manifest push docker.io/$TAG_BASENAME-prod-dbg
-
