57db7edeaaa9945cec8520bd6da07ddea2eede8f
ArmorDarks <git@lavrins.com>
2026-05-07 13:54:26 +0300

    docs(libfakemallinfo): clarify Chromium workaround and document usage

--- docker/build-gstreamer/fake_mallinfo.c
+++ docker/build-gstreamer/fake_mallinfo.c
@@ -1,3 +1,9 @@
+// LD_PRELOAD shim that replaces glibc's deprecated mallinfo() with a stub
+// returning a zeroed struct. Works around a Chromium bug where MemoryInfra
+// crashes with SIGILL once heap addresses exceed 2 GiB, because mallinfo()'s
+// int fields overflow.
+// @see https://github.com/chromiumembedded/cef/issues/3963
+
 #include <malloc.h>
 
 struct mallinfo mallinfo(void) {
